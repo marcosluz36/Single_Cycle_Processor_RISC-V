a915530f410f69d515b734fa1c3b9ad624c6ca8c
Marcos Antônio Inácio Luz <59104567+marcosluz36@users.noreply.github.com>
2025-08-10 23:08:01 -0300

    Update RegisterFile.sv
    RegisterFile updated to display the values of registers x0 through x7, simplifying debugging on the DE2 board.

--- modulos/RegisterFile.sv
+++ modulos/RegisterFile.sv
@@ -1,19 +1,31 @@
 module RegisterFile(
 	input        clk, rst, we3,
-	input  [2:0] wa3, ra1, ra2,
-	input  [7:0] wd3,
-	output [7:0] rd1, rd2
+	input  [4:0] wa3, ra1, ra2,
+	input  [31:0] wd3,
+	output [31:0] rd1, rd2,
+	output [31:0] x0, x1, x2, x3,
+	output [31:0] x4, x5, x6, x7
 );
 
-reg [7:0] register[7:0];
+reg [31:0] register[31:0];
 
 assign rd1 = register[ra1];
 assign rd2 = register[ra2];
 
+assign x0 = register[5'd0];
+assign x1 = register[5'd1];
+assign x2 = register[5'd2];
+assign x3 = register[5'd3];
+assign x4 = register[5'd4];
+assign x5 = register[5'd5];
+assign x6 = register[5'd6];
+assign x7 = register[5'd7];
+
+
 always_ff @ (posedge clk or negedge rst) begin
 	if (!rst) begin
-		for (int i = 0; i < 8; i++)
-			register[i] <= 8'd0;
+		for (int i = 0; i < 32; i++)
+			register[i] <= 32'd0;
 	end
 			
 	else if (we3 && wa3 > 0)
